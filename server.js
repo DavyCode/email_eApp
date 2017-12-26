ace1dbf03a120dfd83858c46d9420e90db196e11
Davy'd <pdaswift3009@gmail.com>
2017-12-26 22:49:54 +0100

    setup passport credentials

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
-const express = require('express'),
+const   express = require('express'),
         app = express(),
+        passport = require('passport'),
+        GoogleStrategy = require('passport-google-oauth20').Strategy
+        keys = require('./config/keys'),
         PORT = process.env.PORT || 3000;
+
+
+passport.use(
+    new GoogleStrategy({
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL :'/auth/google/callback'
+    }, () =>{
+         console.log(accessToken);
+    })
+);
 
 
 app.get('/', (req, res) => {
