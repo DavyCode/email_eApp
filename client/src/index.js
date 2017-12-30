9ec650c8d545c7a37cfc0a4d36218e76d8e96a5c
Davy'd <pdaswift3009@gmail.com>
2017-12-30 15:08:10 +0100

    use router for navigation

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware } from 'redux';
 
 import App from './components/App'
+import reducers from './reducers'
 
-const store = createStore(() => [], {}, applyMiddleware())
+const store = createStore(reducers, {}, applyMiddleware())
 
 render( 
     <Provider store={store}><App /></Provider>, 
