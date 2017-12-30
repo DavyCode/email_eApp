61721df8d9e337ccd4935ee37dbe502358e32d33
Davy'd <pdaswift3009@gmail.com>
2017-12-30 13:30:57 +0100

    create redux store

---
+++ client/src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+           HI React
+        </div>
+    )
+}
+
+export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import App from './components/App'
+
+const store = createStore(() => [], {}, applyMiddleware())
+
+render( 
+    <Provider store={store}><App /></Provider>, 
+    document.querySelector('#root')
+);
+
