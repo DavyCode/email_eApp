ac7422b4897a7ff53758085c7f742bafd636ddcb
Davy'd <pdaswift3009@gmail.com>
2017-12-30 15:46:57 +0100

    include materialize css

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,7 +4,16 @@ class Header extends Component {
   render () {
     return (
       <div>
-        Header
+        <nav>
+          <div className="nav-wrapper">
+            <a href="#" className="left brand-logo">Emaily-Survey</a>
+            <ul id="nav-mobile" className="right hide-on-med-and-down">
+              <li><a href="sass.html">Login</a></li>
+              <li><a href="badges.html">Login with Google</a></li>
+              <li><a href="collapsible.html">Login with Facebook</a></li>
+            </ul>
+          </div>
+        </nav>  
       </div>
     )
   }
