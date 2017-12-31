1a2cc7a981db9581d6ef866bf8c29d9c9b49d1c3
Davy'd <pdaswift3009@gmail.com>
2017-12-31 22:23:50 +0100

    charge user billing

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -3,7 +3,12 @@ const stripe = require('stripe')(keys.stripeSecretKey);
 
 
 module.exports = app => {
-  app.post('/api/stripe', (req, res) => {
-
+  app.post('/api/stripe', async (req, res) => {
+    const charge = await stripe.charges.create({
+      amount: 500,
+      currency: 'usd',
+      description: '$5 for 5 credits',
+      source: 'req.body.id'
+    })
   });
 };
