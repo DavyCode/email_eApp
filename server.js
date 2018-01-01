6345f9db21891d4881eff24cedb01f55745fabb0
Davy'd <pdaswift3009@gmail.com>
2018-01-01 17:45:53 +0100

    prep for deployment

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
 app.use(bodyParser.json());
-app.use(express.static(__dirname + "/public"));
+// app.use(express.static(__dirname + "/public"));
 
 // COOKIE SESSION
 app.use(
