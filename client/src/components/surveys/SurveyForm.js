f656699b90b63a0e1d1a330104848a764a17d762
Davy'd <pdaswift3009@gmail.com>
2018-01-03 23:06:06 +0100

    validate emails

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -2,14 +2,15 @@ import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
 import { Link } from 'react-router-dom'
 import SurveyField from './SurveyField'
+import validateEmails from '../../utils/validateEmails'
 
 
 
 const FIELDS = [
-  { label:"Survey Title", name:'title' },
-  { label:"Subject Line", name:'subject' },
-  { label:"Email Body", name:'body' },
-  { label:"Recipient List", name:'emails' },
+  { label:"Survey Title", name:'title', valueError: 'title is required' },
+  { label:"Subject Line", name:'subject', valueError: 'a subject is required'},
+  { label:"Email Body", name:'body' , valueError: 'body is required'}, 
+  { label:"Recipient List", name:'emails', valueError: 'type in atleast an email'},
 ];
 
 
@@ -46,12 +47,17 @@ class SurveyForm extends  Component {
   }
 };
 
+//validation
 function validate(values) {
   const errors = {};
 
-  if(!values.title) {
-    errors.title = 'Title is required';
-  }
+  errors.emails = validateEmails(values.emails || '');
+
+  FIELDS.forEach( ({ name, valueError }) => {
+    if (!values[name]) {
+      errors[name] = valueError;
+    }
+  });
 
   return errors;
 }
