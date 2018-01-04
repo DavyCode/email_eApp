7388cfce21fe517a934d04d64ac02c6bf7a1cf5a
Davy'd <pdaswift3009@gmail.com>
2018-01-04 19:42:22 +0100

    filter unique feedback events

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -18,10 +18,19 @@ module.exports = app => {
   })
 
   app.post('/api/surveys/webhooks', (req, res) => {
-    const event = _.map(req.body, (event) => {
-      const pathname = new URL(event.url).pathname;
+    const events = _.map(req.body, ({ email, url }) => {
+      const pathname = new URL(url).pathname;
       const p = new Path('/api/surveys/:surveyId/:choice');
+      const match = p.test(pathname);
+      if(match) {
+        return { email, surveyId: match.surveyId, choice: match.choice }
+      }
     });
+
+    const compactEvents = _.compact(events);
+    const uniqueEvents = _.uniqBy(compactEvents, 'email', 'surveyId');
+
+    res.send({});
   });
   
   app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
