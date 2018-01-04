a724594bd73f0fe41910474e1d0ed1475c89beb1
Davy'd <pdaswift3009@gmail.com>
2018-01-04 15:25:43 +0100

    setup webhook localtunnel

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -9,10 +9,15 @@ const Survey = mongoose.model('surveys');
 
 
 module.exports = app => {
+  
   app.get('/api/surveys/feedback', (req, res) => {
     res.send('Thanks for your feedback')
   })
 
+  app.post('/api/surveys/webhooks', (req, res) => {
+    console.log(req.body);
+    res.send({});
+  })
   
   app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
     const { title, subject, body, recipients } = req.body;
---
+++ sendgrid_webhook.js
@@ -0,0 +1,4 @@
+var localtunnel = require('localtunnel');
+localtunnel(3001, { subdomain: adsjgwokcnmesdi }, function(err, tunnel) {
+  console.log('LT running')
+});
